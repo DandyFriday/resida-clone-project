b39a3bcac634e215542f8eabb186c880ee977def
DandyGeorge <dandyfriday31@gmail.com>
2024-06-16 04:12:27 +0100

    contact page added

--- src/index.js
+++ src/index.js
@@ -3,17 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ContactPage from './shared/contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+     
     <BrowserRouter>
-    <App />
-    </BrowserRouter>
     
-  </React.StrictMode>
-);
+    <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/contact' element={<ContactPage />} />
+      </Routes>      
+    
+    </BrowserRouter>    
+ 
+); 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
